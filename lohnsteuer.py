aeac452a22ac232ddc879eb971f5145dbd358d82
Lysuzzz <lysuzzz.xyz@gmail.com>
2024-10-01 19:50:11 +0000

    Test erste Speicherung (nicht vollständig)

---
+++ lohnsteuer.py
@@ -0,0 +1,20 @@
+def get_username():
+    username = input('Bitte geben Sie ihren Nutzernamen ein: ')
+    print(f'Hallo {username}, in den folgenden Schritten werden wir Sie nach Informationen fragen und im Anschluss erhalten Sie ihre Lohnsteuer. Viel Spaß')
+
+def get_informations():
+    print('Hello World')
+
+def generate_lohnsteuer():
+    print('Hello World')
+
+def user_wants_again():
+    print('Hello World')
+
+def create_lohnsteuer():
+    get_username()
+    get_informations()
+    generate_lohnsteuer()
+    user_wants_again()
+
+create_lohnsteuer()
